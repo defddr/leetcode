074575e6d35d64c488a760e88be2931d66dc922d
stecdeng@126.com <stecdeng@126.com>
2018-11-14 17:28:27 +0800

    079 todo

---
+++ Leetcode_cpp/079/079.cpp
@@ -0,0 +1,25 @@
+﻿// 079.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+bool exist(vector<vector<char>>& board, string word) {
+	int m = board.size(); int n = board[0].size();
+	vector<vector<bool>> travel_record(m, vector<bool>(n, false));
+
+
+}
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
