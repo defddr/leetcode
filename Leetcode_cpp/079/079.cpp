8a57fe8f332ef713aff9893c1c42769c98435116
stecdeng@126.com <stecdeng@126.com>
2018-11-14 17:52:03 +0800

    079

--- Leetcode_cpp/079/079.cpp
+++ Leetcode_cpp/079/079.cpp
@@ -7,19 +7,65 @@
 
 using namespace std;
 
+bool Search(vector<vector<char>>& board, vector<vector<bool>>& traveled_record, const string& word, int idx, int x, int y) {
+	if (idx == word.size())
+		return true;
+	if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size())
+		return false;
+	bool res = false;
+	if (board[x][y] == word[idx] && traveled_record[x][y] == false) {
+		traveled_record[x][y] = true;
+		res = Search(board, traveled_record, word, idx+1, x + 1, y) || Search(board, traveled_record, word, idx+1, x - 1, y) ||
+			Search(board, traveled_record, word, idx+1, x, y + 1) || Search(board, traveled_record, word, idx+1, x, y - 1);
+		traveled_record[x][y] = false;
 
-bool exist(vector<vector<char>>& board, string word) {
-	int m = board.size(); int n = board[0].size();
-	vector<vector<bool>> travel_record(m, vector<bool>(n, false));
+		if (res == true)
+			return res;
+	}
 
-
+	return false;
 }
 
 
+bool exist(vector<vector<char>>& board, string word) {
+	int m = board.size(); int n = board[0].size();
+	vector<vector<bool>> traveled_record(m, vector<bool>(n, false));
 
+	bool res = false;
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			int idx = 0;
+			res = Search(board , traveled_record, word,idx,i,j);
+			if (true == res)
+				return res;
+		}
+	}
+
+	return false;
+}
+
+
+/*
+[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+"ABCCED"
+*/
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	vector<vector<char>> board;
+	vector<char> v1{ 'A', 'B', 'C', 'E' };
+	board.push_back(v1);
+
+	vector<char> v2{ 'S', 'F', 'C', 'S' };
+	board.push_back(v2);
+
+	vector<char> v3{ 'A', 'D', 'E', 'E' };
+	board.push_back(v3);
+
+
+	string s = "ABCCED";
+
+    std::cout << exist(board,s) << std::endl;
 }
 
