98c89c81d8d883ad1b9936300264adbdd705f222
stecdeng@126.com <stecdeng@126.com>
2018-11-15 11:36:18 +0800

    590

---
+++ Leetcode_cpp/589/589.cpp
@@ -0,0 +1,45 @@
+﻿// 589.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+	int val;
+	vector<Node*> children;
+
+	Node() {}
+
+	Node(int _val, vector<Node*> _children) {
+		val = _val;
+		children = _children;
+	}
+};
+
+vector<int> g_v;
+
+void preorderInner(Node* root) {
+	if (NULL == root)
+		return;
+	g_v.push_back(root->val);
+	for (const auto& e : root->children) {
+		preorderInner(e);
+	}
+}
+
+vector<int> preorder(Node* root) {
+	preorderInner(root);
+
+	return g_v;
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
---
+++ Leetcode_cpp/590/590.cpp
@@ -0,0 +1,45 @@
+﻿// 590.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+	int val;
+	vector<Node*> children;
+	Node() {}
+	Node(int _val, vector<Node*> _children) {
+		val = _val;
+		children = _children;
+	}
+};
+
+
+vector<int> g_v;
+void preorderInner(Node* root) {
+	if (NULL == root)
+		return;
+	for (const auto& e : root->children) {
+		preorderInner(e);
+	}
+	g_v.push_back(root->val);
+}
+
+
+vector<int> postorder(Node* root) {
+	preorderInner(root);
+	return g_v;
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
